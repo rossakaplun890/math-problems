0983acd245ef55eb9e8d56ba23a3fbbdcb3b4b57
rossakaplun890 <rossakaplun890@gmail.com>
2025-04-01 16:15:40 +0000

    Add 21.ts

---
+++ src/21.ts
@@ -0,0 +1,11 @@
+import sympy as sp
+from sympy import symbols
+
+a, b = symbols('a b')
+x, y = symbols('x y')
+
+expr1 = a*x + b*y - 3*x**2 - 4*a*y + 5*x + 7*y - 20
+expr2 = a*x + b*y - 3*x**2 - 4*a*y + 5*x + 8*y - 21
+
+solve(expr1, (x, y))
+solve(expr2, (x, y))
