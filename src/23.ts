1854be2818932df68723cc15852ab7fba140472d
rossakaplun890 <rossakaplun890@gmail.com>
2025-04-05 06:25:20 +0000

    Add 23.ts

---
+++ src/23.ts
@@ -0,0 +1,10 @@
+import numpy as np
+
+def example_function(x):
+    """
+    This is an example function.
+    You can use this function in your project math-problems.
+    """
+    # Example implementation of a mathematical operation
+    result = x * 2 + np.random.uniform(-1, 1)
+    return result
