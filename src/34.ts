e338621774f23b7221ba005a5433e3e382f24b1f
rossakaplun890 <rossakaplun890@gmail.com>
2025-05-19 09:54:52 +0000

    Add 34.ts

---
+++ src/34.ts
@@ -0,0 +1,8 @@
+import sympy as sp
+from sympy import symbols
+
+x, y = symbols('x y')
+eq1 = x**2 + 2*x*y - 3*y**2
+eq2 = x**4 - 5*x**2*y + 6*y**3
+solution = sp.solve((eq1, eq2), (x, y))
+print(solution)
